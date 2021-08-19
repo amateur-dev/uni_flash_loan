ebbbd18fa9809df25ba29f551bfb43c0914be6d0
amateur_dev <me@dipeshsukhani.dev>
2021-08-19 08:17:40 +0800

    base flash loan contract

---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  hardhat: {
+    forking: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ARCHIVE_NODE}`,
+      blockNumber: 11095000,
+      allowUnlimitedContractSize: false,
+    }
+  },
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000
+      },
+    },
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000
+      }
+    },
+    mocha: {
+      timeout: 20000
+    }
+  }
+}
